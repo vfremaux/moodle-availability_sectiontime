5cdb3439f66861752e137b72d50d53d34d2dadd0
Valery Fremaux <valery.fremaux@gmail.com>
2023-04-26 15:02:17 +0200

    %1

--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2019290300;
-$plugin->requires = 2022041200;
+$plugin->requires = 2022041900;
 $plugin->component = 'availability_sectiontime';
-$plugin->release = '4.0.0 (Build 2019290300)';
+$plugin->release = '4.1.0 (Build 2019290300)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('block_use_stats' => '2019290300');
-$plugin->supported = [40,40];
+$plugin->supported = [40,42];
 
 // Non moodle attribute.
-$plugin->codeincrement = '4.0.0002';
+$plugin->codeincrement = '4.1.0002';
